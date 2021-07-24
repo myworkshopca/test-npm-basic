286a60624464032b9e2b07daa80d667729170fd6
Sean Chen <sean.chen@leocorn.com>
2021-07-24 13:35:44 +0000

    add more cases and some comments

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 'use strict';
 
+// From:
+// https://nodejs.org/en/knowledge/getting-started/what-is-require/
+// - The basic functionality of require is that it reads a JavaScript file,
+//   executes the file, and then proceeds to return the exports object
 const basic = require('seanchen-npm-basic');
+const second = require('seanchen-npm-basic');
 
-basic.setMsg('Hello!');
+// set the message from basic instance
+basic.setMsg('Hello from basic!');
+
+// both instances will have the same message.
+basic.greetingMsg();
+second.greetingMsg();
+
+// both instances are acturally the same.
+console.log(basic === second);
+
+second.setMsg('Hello from second!');
 
 basic.greetingMsg();
+second.greetingMsg();
