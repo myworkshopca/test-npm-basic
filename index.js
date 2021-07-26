8b21854c99e84b431cde3c1d0805b8ea0d512e0a
Sean Chen <sean.chen@leocorn.com>
2021-07-26 13:16:23 +0000

    update to version 1.0.5, add cases for normal classes

--- index.js
+++ index.js
@@ -7,12 +7,19 @@
 const basic = require('seanchen-npm-basic').basicSingleton;
 const second = require('seanchen-npm-basic').basicSingleton;
 
+const normal = require('seanchen-npm-basic').normalClass;
+
+const normalClass = new normal("hello from normal!")
+const normalOne = new normal("hello from normal One!")
+
 // set the message from basic instance
 basic.setMsg('Hello from basic!');
 
 // both instances will have the same message.
 basic.greetingMsg();
 second.greetingMsg();
+normalClass.greetingMsg();
+normalOne.greetingMsg();
 
 // both instances are acturally the same.
 console.log(basic === second);
